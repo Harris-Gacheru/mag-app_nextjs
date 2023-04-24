4309276e2ca5561753ddab069b625c21bc9a1e4c
Harris-Gacheru <harrisgacheru.hg@gmail.com>
2023-04-24 07:03:13 +0300

    Add link to navbar brand

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,9 @@ import Link from "next/link"
 export default function Navbar(){
     return(
         <div className="w-screen h-14 bg-black text-white p-4 flex justify-between items-center">
-            <h1 className="font-bold text-xl">MAG-APP</h1>
+            <h1 className="font-bold text-xl">
+                <Link href={'/'}>MAG-APP</Link>
+            </h1>
             <ul className="flex gap-10">
                 <li>
                     <Link href='/' className="hover:text-red-500">Home</Link>
